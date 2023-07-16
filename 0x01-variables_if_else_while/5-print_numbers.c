11757ee096aef8fd2b94da7c3aa07a5d17d9e147
Joyous07 <joyomamakpor@gmail.com>
2023-07-16 07:02:51 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - function that print digit number
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	while (num < 10)	
+	{
+		printf("%d", num);
+		num += 1;
+	}
+	printf("\n");
+	return (0);
+}
