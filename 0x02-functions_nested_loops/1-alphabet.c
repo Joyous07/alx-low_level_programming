669ea21efdad99bd8dcfaa88087fd821264c07bf
Joyous07 <joyomamakpor@gmail.com>
2023-07-18 15:56:00 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	char j = 'a';
+
+	while (j <= 'z');
+	{
+		_putchar(j);
+		j++;
+		
+	}
+	_putchar('\n');
+}		
