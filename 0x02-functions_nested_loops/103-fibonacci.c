07de7207cc40210aeeefc7c8f515e21b1fa45530
Joyous07 <joyomamakpor@gmail.com>
2023-07-19 18:38:12 -0700

    103 fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,7 +3,7 @@
 /**
  * main - Finds and prints the sum of the sum of the even-valued terms
  *
- * Return:0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
@@ -14,9 +14,9 @@ int main(void)
 	k = 2;
 	sum = 0;
 
-	for (i = 1; 1 <= 33; ++i)
+	for (i = 1; i <= 33; ++i)
 	{
-		if (j < 4000000 && (j % 2) ==0)
+		if (j < 4000000 && (j % 2) == 0)
 		{
 			sum = sum + j;
 		}
