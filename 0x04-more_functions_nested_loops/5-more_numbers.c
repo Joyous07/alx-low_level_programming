85895d7aaca2ba866fe1703cccba22d8d59f973e
Joyous07 <joyomamakpor@gmail.com>
2023-07-20 19:10:11 -0700

    5 more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,10 +12,10 @@ void more_numbers(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; i <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			_putchar('i');
+			_putchar('1');
 			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
