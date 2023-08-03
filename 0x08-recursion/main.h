d3d9538c796bf15f460a8bd034233a26c31fafcf
Joyous07 <joyomamakpor@gmail.com>
2023-08-02 18:48:23 -0700

    100-palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -16,6 +16,8 @@ int sqrt_a(int a, int b);
 int is_prime_number(int n);
 int actual_prime(int n, int i);
 int is_palindrome(char *s);
+int _strlen_recursion(char *s);
+int check_pal(char *s, int i, int len);
 int wildcmp(char *s1, char *s2);
 
 #endif
