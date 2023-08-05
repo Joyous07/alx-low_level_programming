94e5657769a640e76fb6eeb662e9504483a4e8b8
Joyous07 <joyomamakpor@gmail.com>
2023-08-04 18:19:29 -0700

    3 puts

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - function that convert a string to an integer.
+ * @s: Input value.
+ */
+
+int _atoi(char *s)
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string pointed to by src, including
+ * the terminating null byte (\0), to the buffer pointed to by dest.
+ * @dest: Input value.
+ * @src: Input value.
+ * Return: The pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
